b2984d6b89fb72ed68a232adecde579f29895c26
Ashish Datta <ashish@setfive.com>
2014-01-29 23:15:10 -0500

    - removed the bin folder

--- src/com/setfive/Main.scala
+++ src/com/setfive/Main.scala
@@ -9,13 +9,25 @@ import java.util.regex.Pattern
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
+import scala.util.{Try, Success, Failure}
+import scala.util.matching.Regex
 
-case class ParsedEmail(from: String, subject: String, date: String, 
+case class ParsedEmail(from: String, fromUser: String, fromDomain: String, subject: String, date: String, 
 					   labels: List[String], toList: List[String], pk_id: Option[Integer] = None)
 
 object Main {
   
-  val mysqlConn = DriverManager.getConnection("jdbc:mysql://localhost/howrse?user=root&password=root")
+  val mysqlConn = DriverManager.getConnection("jdbc:mysql://localhost/howrse?user=root&password=root&rewriteBatchedStatements=true")
+  
+  val dateParser = new java.text.SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss")
+  val dateOutput = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")  
+  
+  val labelsRe = "(?s).*X-Gmail-Labels: ([\\w,]+)".r
+  val dateRe = """(?s).*Date: ([\w\d\s,:]+)""".r
+  val fromRe = """(?s).*From: .*? <(.*?)>""".r
+  val subjectRe = """(?s).*Subject: (.*?)(?m)^""".r
+  val toRe = """(?s).*To: (.*?)(?m)^""".r
+  val addressesRe = """(?s).*<(.*?)>""".r
   
   def main(args: Array[String]): Unit = {    
     val mboxFile = new FileInputStream( new File("/home/ashish/Downloads/myinbox_small.mbox") )    
@@ -31,69 +43,112 @@ object Main {
 	    	}						 
 	    })
 	    .map(extractEmailInfo)
-	    .map(el => {
-	      val stmt = mysqlConn.prepareStatement( "INSERT INTO email (created_at, email_from, subject) VALUES (?, ?, ?)" )
-	      
-	      stmt.setString(1, el.date)
-	      stmt.setString(2, el.from)
-	      stmt.setString(3, el.subject)
-	      stmt.executeUpdate()	      
-	      	      
-	      val rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
-	      val pk = if(rs.next()){ 
-	        rs.getInt(1) 
-	      }else{
-	        throw new Exception("No auto increment ID?")
-	      }
-	      
-	      el.copy( pk_id = Option[Integer](pk) )
-	    })
-	    
-	println(parsedEmails)
+	    .map(insertEmailRow)
+	    .map(insertEmailMetadataRow)
+	    	    
+	println("Processed: " + parsedEmails.length + " emails.")
 	    
   }
 
+  def insertEmailMetadataRow(el: ParsedEmail): ParsedEmail = {
+	      
+	  this.mysqlConn.setAutoCommit(false)	      
+	  val labelStmt =  this.mysqlConn.prepareStatement( "INSERT INTO email_label (email_id, label) " + "VALUES (?, ?)" )
+	  val toStmt = this.mysqlConn.prepareStatement( "INSERT INTO email_to (email_id, email_to) " + "VALUES (?, ?)" )
+	  
+	  println("Inserting meta for " + el.subject)
+	  
+	  for(label <- el.labels){
+	    labelStmt.setInt(1, el.pk_id.get)
+	    labelStmt.setString(2, label)
+	    labelStmt.addBatch()
+	  }
+	  
+	  for(toAddr <- el.toList){
+	    toStmt.setInt(1, el.pk_id.get)
+	    toStmt.setString(2, toAddr)
+	    toStmt.addBatch()
+	  }
+	  	      
+	  labelStmt.executeBatch()	      
+	  toStmt.executeBatch()
+	  this.mysqlConn.commit()
+	  
+	  el
+  }
+  
+  def insertEmailRow(el: ParsedEmail): ParsedEmail = {
+	  this.mysqlConn.setAutoCommit(true)
+	  val stmt = this.mysqlConn
+			  		 .prepareStatement( "INSERT INTO email (created_at, email_from, subject, email_from_user, email_from_domain) " 
+			  						+ "VALUES (?, ?, ?, ?, ?)" )
+	  
+	  println("Inserting " + el.subject)
+			  						
+	  stmt.setString(1, el.date)
+	  stmt.setString(2, el.from)
+	  stmt.setString(3, el.subject)
+	  stmt.setString(4, el.fromUser)
+	  stmt.setString(5, el.fromDomain)
+	  
+	  stmt.executeUpdate()	      
+	  	      
+	  val rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
+	  val pk = if(rs.next()){ 
+	    rs.getInt(1) 
+	  }else{
+	    throw new Exception("No auto increment ID?")
+	  }
+	  
+	  el.copy( pk_id = Option[Integer](pk) )
+  }
+  
   def extractEmailInfo(el:String): ParsedEmail = {
     
-    val dateParser = new java.text.SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss")
-	val dateOutput = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val labels = this.labelsRe.findFirstMatchIn(el) match {
+    	case Some(m) => m.group(1).split(",").toList
+    	case None => List[String]()
+    }    
 
-    val labels = "(?s).*X-Gmail-Labels: ([\\w,]+)"
-      			.r.findAllIn(el).matchData.map( _.group(1) ).toList
-
-    val date = """(?s).*Date: ([\w\d\s,:]+)""".r.findFirstMatchIn(el) match {
+    val date = this.dateRe.findFirstMatchIn(el) match {
 	    case Some(m) => {	    	    
 	    	try{
-	    		dateOutput.format( dateParser.parse(m.group(1).trim()) )
+	    		this.dateOutput.format( this.dateParser.parse(m.group(1).trim()) )
 	    	}catch {
 	    		case e: Exception => ""
 	    	}	    	    	    	    
 	    }
 	    case None => ""
     }
-
-    val from = """(?s).*From: .*? <(.*?)>""".r.findFirstMatchIn(el) match {
+    
+        
+    val from = this.fromRe.findFirstMatchIn(el) match {
     	case Some(m) => m.group(1).trim()
     	case None => ""
     }
 
-    val subject = """(?s).*Subject: (.*?)(?m)^""".r.findFirstMatchIn(el) match {
+    val fromParts = if (from.indexOf("@") > 0) { from.split("@") } else { Array[String]("", "") }
+    
+    val subject = this.subjectRe.findFirstMatchIn(el) match {
     	case Some(m) => m.group(1).trim()
     	case None => ""
     }    
     
-    val toBlock = """(?s).*To: (.*?)(?m)^""".r.findFirstMatchIn(el) match {
+    val toBlock = this.toRe.findFirstMatchIn(el) match {
     	case Some(m) => m.group(1).trim()
     	case None => ""
     }
 
     val addressList = toBlock.split(",").map(el => {
-    	"""(?s).*<(.*?)>""".r.findAllIn(el).matchData
+    	this.addressesRe.findAllIn(el).matchData
     	.map( _.group(1).trim )
     	.toList.mkString("")
     }).toList
     
-    new ParsedEmail(labels = labels, date = date, subject = subject, from = from, toList = addressList)	    	
-  	}
+    println("Extracted " + subject)
+    
+    new ParsedEmail(labels = labels, date = date, fromUser = fromParts(0), fromDomain = fromParts(1), 
+        			subject = subject, from = from, toList = addressList)	    	
+  }
   
 }
